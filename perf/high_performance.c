5ebe2c437c61363dc3c3fd754a70bd00858e2534
Ea0011 <avagyanee@gmail.com>
2024-11-22 15:24:36 +0400

    add avx512 matmul implementation

--- perf/high_performance.c
+++ perf/high_performance.c
@@ -225,6 +225,56 @@ void matmul_avx_optimized(float* A, float* B_t, float* C, int M, int K, int N) {
     }
 }
 
+void matmul_avx_optimized_loop_unrolling(float* A, float* B_t, float* C, int M, int K, int N) {
+    float* temp = aligned_alloc(ALIGNMNET_SIZE, 8 * sizeof(float));
+    for (int i = 0; i < M; ++i) {
+        for (int j = 0; j < N; ++j) {
+            __m256 sum1 = _mm256_setzero_ps();
+            __m256 sum2 = _mm256_setzero_ps();
+            for (int k = 0; k < K; k += 16) {
+                __m256 a1 = _mm256_load_ps(&A[i * K + k]);   // A[i][k:k+7]
+                __m256 b1 = _mm256_load_ps(&B_t[j * K + k]); // B_t[j][k:k+7]
+                sum1 = _mm256_fmadd_ps(a1, b1, sum1);
+
+                __m256 a2 = _mm256_load_ps(&A[i * K + k + 8]);   // A[i][k:k+7]
+                __m256 b2 = _mm256_load_ps(&B_t[j * K + k + 8]); // B_t[j][k:k+7]
+                sum2 = _mm256_fmadd_ps(a2, b2, sum2);
+            }
+            sum2 = _mm256_add_ps(sum1, sum2);
+            _mm256_store_ps(temp, sum2);
+            float c_sum = temp[0] + temp[1] + temp[2] + temp[3] +
+                          temp[4] + temp[5] + temp[6] + temp[7];
+            C[i * N + j] = c_sum;
+        }
+    }
+}
+
+void matmul_avx_512_optimized_loop_unrolling(float* A, float* B_t, float* C, int M, int K, int N) {
+    float* temp = aligned_alloc(ALIGNMNET_SIZE, 16 * sizeof(float));
+    for (int i = 0; i < M; ++i) {
+        for (int j = 0; j < N; ++j) {
+            __m512 sum1 = _mm512_setzero_ps();
+            __m512 sum2 = _mm512_setzero_ps();
+            for (int k = 0; k < K; k += 32) {
+                __m512 a1 = _mm512_load_ps(&A[i * K + k]);   // A[i][k:k+7]
+                __m512 b1 = _mm512_load_ps(&B_t[j * K + k]); // B_t[j][k:k+7]
+                sum1 = _mm512_fmadd_ps(a1, b1, sum1);
+
+                __m512 a2 = _mm512_load_ps(&A[i * K + k + 16]);   // A[i][k:k+7]
+                __m512 b2 = _mm512_load_ps(&B_t[j * K + k + 16]); // B_t[j][k:k+7]
+                sum2 = _mm512_fmadd_ps(a2, b2, sum2);
+            }
+            sum2 = _mm512_add_ps(sum1, sum2);
+            _mm512_store_ps(temp, sum2);
+            float c_sum = temp[0] + temp[1] + temp[2] + temp[3] +
+                          temp[4] + temp[5] + temp[6] + temp[7] +
+                          temp[8] + temp[9] + temp[10] + temp[11] +
+                          temp[12] + temp[13] + temp[14] + temp[15];
+            C[i * N + j] = c_sum;
+        }
+    }
+}
+
 int main() {
     // Test data
     size_t length = 5000000; // Large vector size for benchmarking
@@ -283,17 +333,28 @@ int main() {
     // Transpose B into B_t
     start = clock();
     transpose_matrix(B, B_t, K, N);
+    end = clock();
+    printf("transpose time: %f seconds\n", ((double)(end - start) / CLOCKS_PER_SEC));
 
+    start = clock();
     matmul_single_thread(A, B_t, C, M, K, N);
     end = clock();
     printf("matmul single thread time: %f seconds\n", ((double)(end - start) / CLOCKS_PER_SEC));
 
     start = clock();
-    transpose_matrix(B, B_t, K, N);
-
     matmul_avx_optimized(A, B_t, C, M, K, N);
     end = clock();
     printf("matmul avx time: %f seconds\n", ((double)(end - start) / CLOCKS_PER_SEC));
+
+    start = clock();
+    matmul_avx_optimized_loop_unrolling(A, B_t, C, M, K, N);
+    end = clock();
+    printf("matmul avx unrolled loop time: %f seconds\n", ((double)(end - start) / CLOCKS_PER_SEC));
+
+    start = clock();
+    matmul_avx_512_optimized_loop_unrolling(A, B_t, C, M, K, N);
+    end = clock();
+    printf("matmul avx512 unrolled loop time: %f seconds\n", ((double)(end - start) / CLOCKS_PER_SEC));
 
     return 0;
 }
