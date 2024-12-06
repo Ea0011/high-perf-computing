6f9ab72f2e02eff8f6c6c118f086a03e74f34c72
Ea0011 <avagyanee@gmail.com>
2024-12-07 00:12:33 +0400

    fix timing for multithreaded versions and add omp compilation

--- mlp/mlp.c
+++ mlp/mlp.c
@@ -37,7 +37,7 @@ void matmul_single_thread(float* A, float* B_t, float* C, int M, int K, int N) {
 
 void matmul_parallel_simd(float* A, float* B_t, float* C, float* bias, int M, int K, int N) {
     for (int i = 0; i < M; ++i) {
-        #pragma omp parallel for num_threads(4) schedule(static)
+        #pragma omp parallel for num_threads(6) schedule(static)
         for (int j = 0; j < N; ++j) {
             float sum = bias[j];
 
@@ -51,7 +51,7 @@ void matmul_parallel_simd(float* A, float* B_t, float* C, float* bias, int M, in
 }
 
 void matmul_avx_optimized(float* A, float* B_t, float* C, int M, int K, int N) {
-    float* temp = aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), 8);
+    float* temp = (float*)aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), 8);
     for (int i = 0; i < M; ++i) {
         for (int j = 0; j < N; ++j) {
             __m256 sum = _mm256_setzero_ps();
@@ -69,7 +69,7 @@ void matmul_avx_optimized(float* A, float* B_t, float* C, int M, int K, int N) {
 }
 
 void matmul_avx_optimized_loop_unrolling(float* A, float* B_t, float* C, int M, int K, int N) {
-    float* temp = aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), 8);
+    float* temp = (float*)aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), 8);
     for (int i = 0; i < M; ++i) {
         for (int j = 0; j < N; ++j) {
             __m256 sum1 = _mm256_setzero_ps();
@@ -93,28 +93,23 @@ void matmul_avx_optimized_loop_unrolling(float* A, float* B_t, float* C, int M, 
 }
 
 void fused_matmul_bias_relu_avx(float* A, float* B_t, float* C, float* bias, int M, int K, int N) {
-    float* temp = aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), 8);
+    float* temp = (float*)aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), 8);
     for (int i = 0; i < M; ++i) {
         for (int j = 0; j < N; ++j) {
-            __m256 sum1 = _mm256_set1_ps(bias[j] / 8);
-            __m256 sum2 = _mm256_setzero_ps();
-            for (int k = 0; k < K; k += 16) {
-                __m256 a1 = _mm256_load_ps(&A[i * K + k]);   // A[i][k:k+7]
-                __m256 b1 = _mm256_load_ps(&B_t[j * K + k]); // B_t[j][k:k+7]
-                sum1 = _mm256_fmadd_ps(a1, b1, sum1);
-
-                __m256 a2 = _mm256_load_ps(&A[i * K + k + 8]);   // A[i][k:k+7]
-                __m256 b2 = _mm256_load_ps(&B_t[j * K + k + 8]); // B_t[j][k:k+7]
-                sum2 = _mm256_fmadd_ps(a2, b2, sum2);
+            __m256 sum = _mm256_setzero_ps();
+            for (int k = 0; k < K; k += 8) {
+                __m256 a = _mm256_load_ps(&A[i * K + k]);   // A[i][k:k+7]
+                __m256 b = _mm256_load_ps(&B_t[j * K + k]); // B_t[j][k:k+7]
+                sum = _mm256_fmadd_ps(a, b, sum);
             }
-            sum2 = _mm256_add_ps(sum1, sum2);
-            _mm256_store_ps(temp, sum2);
+            _mm256_store_ps(temp, sum);
             float c_sum = temp[0] + temp[1] + temp[2] + temp[3] +
-                          temp[4] + temp[5] + temp[6] + temp[7];
+                        temp[4] + temp[5] + temp[6] + temp[7] + bias[j];
             C[i * N + j] = c_sum > 0 ? c_sum : 0;
         }
     }
 }
+
 
 // ReLU activation function
 void relu(float* input, int size) {
@@ -143,8 +138,8 @@ void mlp(
     int output_dim,
     int hidden_size
 ) {
-    float* x = aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), batch_size * hidden_size);
-    float* temp = aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), batch_size * hidden_size);
+    float* x = (float*)aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), batch_size * hidden_size);
+    float* temp = (float*)aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), batch_size * hidden_size);
 
     matmul_single_thread(input, input_layer_weights, x, batch_size, input_dim, hidden_size);
     for (int l = 0; l < num_layers; ++l) {
@@ -180,8 +175,8 @@ void mlp_multithread_simd(
     int output_dim,
     int hidden_size
 ) {
-    float* x = aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), batch_size * hidden_size);
-    float* temp = aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), batch_size * hidden_size);
+    float* x = (float*)aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), batch_size * hidden_size);
+    float* temp = (float*)aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), batch_size * hidden_size);
 
     matmul_single_thread(input, input_layer_weights, x, batch_size, input_dim, hidden_size);
     for (int l = 0; l < num_layers; ++l) {
@@ -211,8 +206,8 @@ void mlp_avx(
     int output_dim,
     int hidden_size
 ) {
-    float* x = aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), batch_size * hidden_size);
-    float* temp = aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), batch_size * hidden_size);
+    float* x = (float*)aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), batch_size * hidden_size);
+    float* temp = (float*)aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), batch_size * hidden_size);
 
     matmul_avx_optimized_loop_unrolling(input, input_layer_weights, x, batch_size, input_dim, hidden_size);
     for (int l = 0; l < num_layers; ++l) {
@@ -274,15 +269,15 @@ int main() {
     int num_layers = 32;
 
     // Allocate memory
-    float* input = aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), B * input_dim);
+    float* input = (float*)aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), B * input_dim);
 
     // Network parameters
-    float* embed_layer = aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), input_dim * hidden_size);
-    float* weights = aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), num_layers * hidden_size * hidden_size);
-    float* biases = aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), num_layers * hidden_size);
-    float* output_layer = aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), hidden_size * output_dim);
+    float* embed_layer = (float*)aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), input_dim * hidden_size);
+    float* weights = (float*)aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), num_layers * hidden_size * hidden_size);
+    float* biases = (float*)aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), num_layers * hidden_size);
+    float* output_layer = (float*)aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), hidden_size * output_dim);
 
-    float* output = aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), B * output_dim);
+    float* output = (float*)aligned_allocate_buffer(ALIGNMENT_SIZE, sizeof(float), B * output_dim);
 
     initialize_matrix(input, B, input_dim);
     for (int l = 0; l < num_layers; l++) {
